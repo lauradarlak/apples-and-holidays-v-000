e75e20c616e387d80c9986cd3db954ea4b82f92c
Laura Darlak <laura@polyporedesign.com>
2018-08-21 20:53:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,8 +60,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  return holiday_hash[:winter].values.flatten
-  
+  binding.pry
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
